28d0e3fa51d3dc517e23351e18013c40d54ced15
Ricardo <120153575+RamuneSodaa@users.noreply.github.com>
2026-04-07 22:06:54 -0400

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,19 @@
-import 'reflect-metadata';
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { HttpErrorFilter } from './common/http-error.filter';
+# Terminal Environment Changes
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.useGlobalFilters(new HttpErrorFilter());
-  await app.listen(3000);
-}
+## Extension: vscode.git
 
-bootstrap();
+Enables the following features: git auth provider
+
+- `GIT_ASKPASS=/Applications/Cursor.app/Contents/Resources/app/extensions/git/dist/askpass.sh`
+- `VSCODE_GIT_ASKPASS_NODE=/Applications/Cursor.app/Contents/Frameworks/Cursor Helper (Plugin).app/Contents/MacOS/Cursor Helper (Plugin)`
+- `VSCODE_GIT_ASKPASS_EXTRA_ARGS=`
+- `VSCODE_GIT_ASKPASS_MAIN=/Applications/Cursor.app/Contents/Resources/app/extensions/git/dist/askpass-main.js`
+- `VSCODE_GIT_IPC_HANDLE=/var/folders/th/3w57j08s2ln7mndvdgz94kd40000gn/T/vscode-git-59ea1bd1e4.sock`
+- `VSCODE_GIT_IPC_AUTH_TOKEN=534b1184a3e9e8904127461bd8a4cc000b46ca0401f3b07519e554edadc510a9`
+
+## Extension: eamodio.gitlens
+
+Enables GK CLI integration
+
+- `GK_GL_ADDR=http://127.0.0.1:57435`
+- `GK_GL_PATH=/var/folders/th/3w57j08s2ln7mndvdgz94kd40000gn/T/gitkraken/gitlens/gitlens-ipc-server-26546-57435.json`
